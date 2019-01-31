b8e288df5bd65deb0fe5684bf6a0e9bff01ec715
Anna-Lena Marx <amarx@inovex.de>
2019-01-31 15:26:53 +0100

    Added a FIDL variant for PDev Grove-LCD drivers
    It is (as far as known and found in the sources) not possible to
    implement more than one FIDL definition in one driver (driver, not user
    app!). So we added a third FIDL definition for PDev drivers which
    just combines the RGB and the LCD one.

--- system/fidl/zircon-display-grove/rules.mk
+++ system/fidl/zircon-display-grove/rules.mk
@@ -25,3 +25,16 @@ MODULE_FIDL_LIBRARY := zircon.display.grove.lcd
 MODULE_SRCS += $(LOCAL_DIR)/grove-lcd.fidl
 
 include make/module.mk
+
+
+MODULE := $(LOCAL_DIR).pdev
+
+MODULE_TYPE := fidl
+
+MODULE_PACKAGE := fidl
+
+MODULE_FIDL_LIBRARY := zircon.display.grove.pdev
+
+MODULE_SRCS += $(LOCAL_DIR)/grove.fidl
+
+include make/module.mk
