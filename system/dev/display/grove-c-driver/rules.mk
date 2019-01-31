a6ac0777f41525072adfbcea0c36bd10f4231731
Anna-Lena Marx <amarx@inovex.de>
2019-01-31 15:45:44 +0100

    Change FIDL lib include to the new PDev version

--- system/dev/display/grove-c-driver/rules.mk
+++ system/dev/display/grove-c-driver/rules.mk
@@ -1,41 +1,41 @@
-LOCAL_DIR := $(GET_LOCAL_DIR)
-
-
-# Makefile for the RGB driver
-MODULE := $(LOCAL_DIR).rgb
-
-MODULE_NAME := grove-rgb-drv
-
-MODULE_TYPE = driver
-
-MODULE_SRCS := $(LOCAL_DIR)/grove-rgb.c
-
-MODULE_STATIC_LIBS := system/ulib/ddk system/ulib/fidl system/ulib/sync
-
-MODULE_LIBS := system/ulib/driver system/ulib/c system/ulib/zircon
-
-MODULE_BANJO_LIBS := system/banjo/ddk-protocol-i2c system/banjo/ddk-protocol-platform-device system/banjo/ddk-protocol-platform-bus
-
-MODULE_FIDL_LIBS := system/fidl/zircon-display-grove.rgb
-
-include make/module.mk
-
-
-# Makefile for the LCD driver
-MODULE := $(LOCAL_DIR).lcd
-
-MODULE_NAME := grove-lcd-drv
-
-MODULE_TYPE = driver
-
-MODULE_SRCS := $(LOCAL_DIR)/grove-lcd.c
-
-MODULE_STATIC_LIBS := system/ulib/ddk system/ulib/fidl system/ulib/sync
-
-MODULE_LIBS := system/ulib/driver system/ulib/c system/ulib/zircon
-
-MODULE_BANJO_LIBS := system/banjo/ddk-protocol-i2c system/banjo/ddk-protocol-platform-device system/banjo/ddk-protocol-platform-bus
-
-MODULE_FIDL_LIBS := system/fidl/zircon-display-grove.lcd
-
-include make/module.mk
+# LOCAL_DIR := $(GET_LOCAL_DIR)
+# 
+# 
+# # Makefile for the RGB driver
+# MODULE := $(LOCAL_DIR).rgb
+# 
+# MODULE_NAME := grove-rgb-drv
+# 
+# MODULE_TYPE = driver
+# 
+# MODULE_SRCS := $(LOCAL_DIR)/grove-rgb.c
+# 
+# MODULE_STATIC_LIBS := system/ulib/ddk system/ulib/fidl system/ulib/sync
+# 
+# MODULE_LIBS := system/ulib/driver system/ulib/c system/ulib/zircon
+# 
+# MODULE_BANJO_LIBS := system/banjo/ddk-protocol-i2c system/banjo/ddk-protocol-platform-device system/banjo/ddk-protocol-platform-bus
+# 
+# MODULE_FIDL_LIBS := system/fidl/zircon-display-grove.rgb
+# 
+# include make/module.mk
+# 
+# 
+# # Makefile for the LCD driver
+# MODULE := $(LOCAL_DIR).lcd
+# 
+# MODULE_NAME := grove-lcd-drv
+# 
+# MODULE_TYPE = driver
+# 
+# MODULE_SRCS := $(LOCAL_DIR)/grove-lcd.c
+# 
+# MODULE_STATIC_LIBS := system/ulib/ddk system/ulib/fidl system/ulib/sync
+# 
+# MODULE_LIBS := system/ulib/driver system/ulib/c system/ulib/zircon
+# 
+# MODULE_BANJO_LIBS := system/banjo/ddk-protocol-i2c system/banjo/ddk-protocol-platform-device system/banjo/ddk-protocol-platform-bus
+# 
+# MODULE_FIDL_LIBS := system/fidl/zircon-display-grove.lcd
+# 
+# include make/module.mk
--- system/dev/display/grove-c-pdev-driver/rules.mk
+++ system/dev/display/grove-c-pdev-driver/rules.mk
@@ -1,7 +1,7 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 
-# Makefile for the RGB driver
+# Makefile for the PDev driver
 MODULE := $(LOCAL_DIR)
 
 MODULE_NAME := grove-pdev-drv
@@ -16,6 +16,7 @@ MODULE_LIBS := system/ulib/driver system/ulib/c system/ulib/zircon
 
 MODULE_BANJO_LIBS := system/banjo/ddk-protocol-i2c system/banjo/ddk-protocol-platform-device system/banjo/ddk-protocol-platform-bus
 
-MODULE_FIDL_LIBS := system/fidl/zircon-display-grove-rgb
+MODULE_FIDL_LIBS := system/fidl/zircon-display-grove.pdev
+
 
 include make/module.mk
